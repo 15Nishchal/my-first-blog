2f71809660851c72e753c8af97aba148180e1d9b
Nishchal <nischal.15gupta@gmail.com>
2019-02-20 09:57:28 -0500

    Modified templates to display posts from database.

---
+++ python_intro.py
@@ -0,0 +1,3 @@
+Post.objects.create(author=me, title='My first post', text='This is my first post on about my first beloved programming language. In this post I have shared my hands on experience over my first programming language')
+
+Post.objects.create(author=me, title='My second post', text='This is my second post on about health.It unfolds the information on about having a good health and strong mind')
